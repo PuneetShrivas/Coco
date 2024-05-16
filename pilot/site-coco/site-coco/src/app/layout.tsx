821c1bc1a9a4ec55848401436e503555eb4915a0
Puneet Shrivas <puneetshrivas32@gmail.com>
2024-05-16 18:45:57 +0530

    added vercel analytics

--- pilot/site-coco/site-coco/src/app/layout.tsx
+++ pilot/site-coco/site-coco/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -26,7 +27,9 @@ export default function RootLayout({
         inter.className 
       )}>
         <Navbar/>
-        {children}</body>
+        {children}
+        <Analytics/>
+        </body>
         </Providers>
     </html>
   );
